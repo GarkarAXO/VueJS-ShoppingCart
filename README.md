804532d63a6874bae2543bbfbeb0ae48e6f48974
GarkarAXO <99782340+GarkarAXO@users.noreply.github.com>
2023-10-04 18:23:29 +0000

    ⭐Feature: Mejorando con keyup

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
     src="/workspaces/VueJS-ShoppingCart/public/img/itgam paint.png"
     alt="Itgam Header" />
 </center>
-# Shopping Cart App
+
+# Shopping Cart 🛒 Project
 
 Proyecto para el curso de desarrollo de aplicaciones  
 para dispositivos moviles 2023-B usando Vue.js
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <script setup>
-
 import { ref } from 'vue';
-const header = ref('App Lista de Compras');
+const header = ref('App Lista de compras');
 const items = ref([
-    {id: 1, label: '10 bolillos'},
-    {id: 2, label: '1 lata de frijoles'},
-    {id: 3, label: '2 lata de atún'},
+  {id: 1, label: '10 bolillos'},
+  {id: 2, label: '1 lata de frijoles'},
+  {id: 3, label: '2 lata de atún'}
 ]);
-
+const newItem=ref('');
+const newItemHighPriority=ref(false);
 </script>
 
 <template>
-  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+  <h1> <i class = "material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+  <div class="add-item form">
+    <!-- Input de nuevo articulo -->
+      <input v-on:keyup.enter="items.push({id: items.length + 1, label: newItem})" v-model.trim="newItem" type="text" placeholder="Ingresar articulo">
+      <!-- Check Boxes -->
+      <label >
+        <input v-model="newItemHighPriority" type="checkbox">
+        Alta Prioridad
+      </label>
+      {{ newItemHighPriority  ? "👍" : "👈"}}
+      <!-- boton en la UI -->
+      <button v-on:click="items.push({id: items.length + 1, label: newItem})" class="btn btn-primary">Salvar articulo</button>
+  </div>
   <ul>
-    <li v-for="{ id, label } in items" v-bind:key="id">🔹 {{label}}</li>
+    <li v-for="{ id, label } in items" v-bind:key="id">
+      🔹 {{ label }}
+    </li>
   </ul>
 </template>
 
-<style scoped>
+<style>
 .shopping-cart-icon {
-  font-size: 2rem;
+  font-size: 2rem; 
 }
 </style>
